f6dda7bc1a38abbeca31e1d4469ed4a2afc5715a
Lucas <sianolucasdev@hotmail.com>
2024-08-29 11:32:44 -0300

    se elimina carpeta de ejemplos

--- ejemplos/propiedades/UnBean.java
+++
@@ -1,9 +0,0 @@
-package es.dsrroma.school.springboot.reuniones;
-
-import org.springframework.beans.factory.annotation.Value;
-
-public class UnBean {
-
-	@Value("${nombre}")
-    private String nombre;
-}
