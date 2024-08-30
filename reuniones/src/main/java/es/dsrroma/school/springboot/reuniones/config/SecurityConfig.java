2967aea33a1ff3568ed635caaa052a3eedce7fc3
Lucas <sianolucasdev@hotmail.com>
2024-08-30 13:16:35 -0300

    Spring actuator

--- reuniones/src/main/java/es/dsrroma/school/springboot/reuniones/config/SecurityConfig.java
+++ reuniones/src/main/java/es/dsrroma/school/springboot/reuniones/config/SecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // Indica que esta clase define beans de configuración para Spring.
@@ -18,13 +22,23 @@ public class SecurityConfig {
         // Configuración de seguridad HTTP
         http.authorizeHttpRequests(
                 authorizeRequests -> authorizeRequests
-                        .requestMatchers("/api/**").hasRole("API_USER") // Las solicitudes a rutas que empiezan con "/api/" requieren el rol "API_USER".
-                        .anyRequest().authenticated()) // Cualquier otra solicitud debe estar autenticada.
+                        .requestMatchers("/api/**", "/api/rest/**").hasRole("API_USER") // Las solicitudes a rutas que empiezan con "/api/" requieren el rol "API_USER".
+                        .anyRequest().permitAll()) // Cualquier otra solicitud debe estar autenticada.
                 .formLogin(
                         formLogin -> formLogin.loginPage("/login").permitAll())
                         ; // Si el usuario no está autenticado, será redirigido a la página de login "/login".
 
         return http.build(); // Construye y devuelve la configuración de seguridad.
+    }
+
+     @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("admin")
+            .password(passwordEncoder().encode("admin123"))  // Configura el usuario "admin" con la contraseña "admin123".
+            .roles("USER", "API_USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean // Define un bean para el codificador de contraseñas.
--- reuniones/src/main/java/es/dsrroma/school/springboot/reuniones/data/ReunionRepository.java
+++ reuniones/src/main/java/es/dsrroma/school/springboot/reuniones/data/ReunionRepository.java
@@ -1,11 +1,11 @@
 package es.dsrroma.school.springboot.reuniones.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import es.dsrroma.school.springboot.reuniones.models.Reunion;
 
-@Repository
+@RepositoryRestResource(path = "reuniones")
 public interface ReunionRepository extends JpaRepository<Reunion, Long>{
 
 
